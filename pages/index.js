e36a68809a932a9f4bab636b4fd4c3d4b517f409
Derek <derekcrosslu@gmail.com>
2021-12-24 14:18:39 -0500

    google maps

--- pages/index.js
+++ pages/index.js
@@ -12,44 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d96743.25988892588!2d-74.16163504463654!3d40.735033160790664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x89c252e1c5ec0cef%3A0xb3f3b437c5d7f286!2sAeropuerto%20Internacional%20Libertad%20de%20Newark%2C%203%20Brewster%20Rd%2C%20Newark%2C%20NJ%2007114!3m2!1d40.6895314!2d-74.1744624!4m5!1s0x89c259ae15b2adcb%3A0x7955420634fd7eba!2sPenn%20Station%2C%20Nueva%20York!3m2!1d40.750568!2d-73.99351899999999!5e0!3m2!1ses-419!2sus!4v1640373085211!5m2!1ses-419!2sus" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
       </main>
 
       <footer className={styles.footer}>
